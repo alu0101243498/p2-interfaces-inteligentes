a5fc7931f7e594f7e55d63a9010a2b2cb78d18ba
alu0101243498 <72921642+alu0101243498@users.noreply.github.com>
2021-10-25 17:35:51 +0100

    All done!!

--- playerMove.cs
+++ playerMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class playerMove : MonoBehaviour
 {
-    public float speed = .1f;
+    public float speed = 0.1f;
     private Rigidbody rigid;
     private int puntuacion = 0;
     private GameObject[] cylinderA;
@@ -21,10 +21,31 @@ public class playerMove : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate() {
         // Movimiento del personaje
-        float xDirection = Input.GetAxis("Horizontal");
+        /*float xDirection = Input.GetAxis("Horizontal");
         float zDirection = Input.GetAxis("Vertical");
         Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
-        rigid.AddForce(moveDirection * speed);
+        rigid.AddForce(moveDirection * speed);*/
+
+        if (Input.GetKey(KeyCode.W)) {
+            Vector3 playerPosition = transform.position;
+            playerPosition.z += speed;
+            transform.position = playerPosition;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            Vector3 playerPosition = transform.position;
+            playerPosition.x -= speed;
+            transform.position = playerPosition;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            Vector3 playerPosition = transform.position;
+            playerPosition.x += speed;
+            transform.position = playerPosition;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            Vector3 playerPosition = transform.position;
+            playerPosition.z -= speed;
+            transform.position = playerPosition;
+        }
 
         /*
         // Cilindros que se alejan al acercarse el jugador
@@ -33,15 +54,6 @@ public class playerMove : MonoBehaviour
             if (range < minDistance) {
                 transform.position = Vector3.MoveTowards(i.transform.position, transform.position, -1 * speedRun * Time.deltaTime);
                 transform.position = -Vector3.MoveTowards(transform.position, i.transform.position, speedRun * Time.deltaTime);
-            }
-        }*/
-
-        /*
-        // Al pulsar espacio lo mueve hacia fuera de él
-        corrector = new Vector3(3f, 0f, 3f);
-        if (Input.GetKey(KeyCode.Space)) {
-            foreach(GameObject i in cylinderA) {
-                i.transform.position = transform.position + corrector; 
             }
         }*/
     }
